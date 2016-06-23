e2e0e76f8f49b2932a68d5cfd78bf16216f56620
OkYesAnap <OkYesAnap@gmail.com>
2016-06-23 21:16:22 +0100

    * This is my car

--- findfile.php
+++ findfile.php
@@ -1,3 +1,9 @@
 <php?
 print"Where are you?";
+
+$myCar = array('Jiguli', array('model'=>'2106','engine'=>'1300')); 
+
+echo 'Car Maker:'.$myCar[0];
+echo 'Model'.$myCar[1][0];
+echo 'Engine copacoty:'.$myCar[1][1];
 ?>
